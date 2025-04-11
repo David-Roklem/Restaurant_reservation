f75e2fa8f93c13e51c0c07300f20fa6a9cf5b3ae
David-Roklem <roklem@mail.ru>
2025-04-11 10:30:46 +0300

    dockerize the app

--- app/main.py
+++ app/main.py
@@ -8,4 +8,4 @@ app.include_router(table.router)
 app.include_router(reservation.router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", host="api", reload=True)
